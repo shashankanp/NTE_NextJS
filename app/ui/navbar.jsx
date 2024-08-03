ecd6454dc7523509ce2e158f61512377e6e4e6e9
N P Shashanka Rao <shashanknp02@gmail.com>
2024-08-03 14:00:36 +0530

    ENH: Navbar almost finished.Mobile responsiveness needs to be fixed

--- app/ui/navbar.jsx
+++ app/ui/navbar.jsx
@@ -22,62 +22,55 @@ export default function Navbar() {
     >
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="flex h-16 justify-between">
-              <div className="flex">
-                <div className="-ml-2 mr-2 flex items-center md:hidden">
-                  {/* Mobile menu button */}
-                  <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                    <span className="absolute -inset-0.5" />
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </DisclosureButton>
-                </div>
-                <div className="flex flex-shrink-0 items-center mr-auto">
-                  <Image
-                    src={Logo}
-                    className="h-8 w-8"
-                    alt="Your Company"
-                    width={500}
-                    height={500}
-                  />
-                </div>
-                <div className="hidden md:ml-6 md:flex md:space-x-8">
+          <div className="mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex h-20 items-center font-bold text-black">
+              <div className="-ml-2 mr-2 flex items-center md:hidden">
+                {/* Mobile menu button */}
+                <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2  focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </DisclosureButton>
+              </div>
+              {/* Left side with logo */}
+              <div className="flex items-center">
+                <Image src={Logo} className="h-14 w-14" alt="NTE Logo" />
+              </div>
+
+              {/* Right side with navigation links */}
+              <div className="ml-auto flex items-center space-x-8">
+                <div className="hidden md:flex md:space-x-8">
                   <a
                     href="#"
-                    className="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900"
+                    className="inline-flex items-center px-1 pt-1 text-sm font-bold "
                   >
                     HOME
                   </a>
                   <a
                     href="#"
-                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                    className="inline-flex items-center border-transparent px-1 pt-1 text-sm font-bold"
                   >
                     OUR TEAM
                   </a>
-                  <Menu as="div" className="relative ml-3">
-                    <div>
-                      <MenuButton className="relative flex rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        <span className="absolute -inset-1.5" />
-                        <span className="sr-only">Open user menu</span>
-                        2021 Conference
-                      </MenuButton>
-                    </div>
+                  <Menu as="div" className="relative">
+                    <MenuButton className="relative  px-1 pt-1 flex rounded-full text-sm focus:outline-none ">
+                      2021 CONFERENCE &#11167;
+                    </MenuButton>
                     <MenuItems
                       transition
-                      className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                      className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-transparent py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
                     >
                       <MenuItem>
                         {({ focus }) => (
                           <a
                             href="#"
                             className={classNames(
-                              focus ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              focus ? "bg-[#dd007a1a]" : "",
+                              "block px-4 py-2 text-sm text-black"
                             )}
                           >
                             CONFERENCE AGENDA
@@ -89,8 +82,8 @@ export default function Navbar() {
                           <a
                             href="#"
                             className={classNames(
-                              focus ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              focus ? "bg-[#dd007a1a]" : "",
+                              "block px-4 py-2 text-sm text-black"
                             )}
                           >
                             CONFERENCE FAQs
@@ -102,8 +95,8 @@ export default function Navbar() {
                           <a
                             href="#"
                             className={classNames(
-                              focus ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              focus ? "bg-[#dd007a1a]" : "",
+                              "block px-4 py-2 text-sm text-black"
                             )}
                           >
                             CONFERENCE RECORDING
@@ -114,13 +107,13 @@ export default function Navbar() {
                   </Menu>
                   <a
                     href="#"
-                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                    className="inline-flex items-center border-transparent px-1 pt-1 text-sm font-bold"
                   >
                     MENTAL HEALTH RESOURCES
                   </a>
                   <a
                     href="#"
-                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                    className="inline-flex items-center border-transparent px-1 pt-1 text-sm font-bold"
                   >
                     BUTTERFLY CHAT
                   </a>
@@ -134,83 +127,38 @@ export default function Navbar() {
               <DisclosureButton
                 as="a"
                 href="#"
-                className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700 sm:pl-5 sm:pr-6"
+                className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-bold text-indigo-700 sm:pl-5 sm:pr-6"
               >
-                Dashboard
+                HOME
               </DisclosureButton>
               <DisclosureButton
                 as="a"
                 href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-bold hover:bg-gray-50 sm:pl-5 sm:pr-6"
               >
-                Team
+                OUR TEAM
               </DisclosureButton>
               <DisclosureButton
                 as="a"
                 href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-bold hover:bg-gray-50 sm:pl-5 sm:pr-6"
               >
-                Projects
+                2021 Conference
               </DisclosureButton>
               <DisclosureButton
                 as="a"
                 href="#"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-bold hover:bg-gray-50 sm:pl-5 sm:pr-6"
               >
-                Calendar
+                MENTAL HEALTH RESOURCES
               </DisclosureButton>
-            </div>
-            <div className="border-t border-gray-200 pb-3 pt-4">
-              <div className="flex items-center px-4 sm:px-6">
-                <div className="flex-shrink-0">
-                  <Image
-                    className="h-10 w-10 rounded-full"
-                    src={Logo}
-                    alt=""
-                    width={500}
-                    height={500}
-                  />
-                </div>
-                <div className="ml-3">
-                  <div className="text-base font-medium text-gray-800">
-                    Tom Cook
-                  </div>
-                  <div className="text-sm font-medium text-gray-500">
-                    tom@example.com
-                  </div>
-                </div>
-                <button
-                  type="button"
-                  className="relative ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-              </div>
-              <div className="mt-3 space-y-1">
-                <DisclosureButton
-                  as="a"
-                  href="#"
-                  className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
-                >
-                  Your Profile
-                </DisclosureButton>
-                <DisclosureButton
-                  as="a"
-                  href="#"
-                  className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
-                >
-                  Settings
-                </DisclosureButton>
-                <DisclosureButton
-                  as="a"
-                  href="#"
-                  className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
-                >
-                  Sign out
-                </DisclosureButton>
-              </div>
+              <DisclosureButton
+                as="a"
+                href="#"
+                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-bold hover:bg-gray-50 sm:pl-5 sm:pr-6"
+              >
+                BUTTERFLY CHAT
+              </DisclosureButton>
             </div>
           </DisclosurePanel>
         </>
